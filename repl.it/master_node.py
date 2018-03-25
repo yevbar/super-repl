c47f74a25c63e987a4c55a45d277334a08c3049a
Yev <yevgeniybarkalov@yahoo.com>
2018-03-25 09:03:14 -0700

    things are finally working!

--- repl.it/master_node.py
+++ repl.it/master_node.py
@@ -1,16 +1,26 @@
-print "Hello from Flask!"
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, url_for
+import logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+import random
 app = Flask(__name__)
 
+string_array = ["hello", "hi", "hey"]
 
 @app.route("/", methods=["GET","POST"])
 def hello():
   if request.method == "GET":
-    return jsonify(continue_="true",action_="import requests\nr = requests.post('https://MasterNodeCool--yevbar.repl.co', json={'ip' : ''})")
+    new_string = random.choice(string_array)
+    return jsonify(continue_="true",action_="import random\ncur_ip = requests.get('http://icanhazip.com/').text.strip()\nprint '" + new_string + "'\nr = requests.post('https://MasterNode--yevbar.repl.co/', json={'ip':cur_ip, 'message':'" + new_string + "'})")
   else:
     print request.get_json(silent=True)
     return "Thanks!"
+
+@app.route("/<ip>")
+def get_ip(ip):
+  print ip
+  return "woot got your ip!"
 
 if __name__ == "__main__":
     app.run(host= '0.0.0.0', port=9000, debug=False)
