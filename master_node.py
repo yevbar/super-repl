c7746efa9353d30d1f353ddc5acef3eb0a8bc75e
Yev <yevgeniybarkalov@yahoo.com>
2018-03-25 05:31:41 -0700

    made initial command more presentable

--- master_node.py
+++ master_node.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return jsonify(continue_="true",action_="print ('This is not a DDOS!')")
+    return jsonify(continue_="true",action_="print ('Hello from Yev and Zane!')")
 
 if __name__ == "__main__":
     app.run(host= '0.0.0.0', port=9000, debug=False)
